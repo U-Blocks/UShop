8d79f57ee808d59486ed26d43825b1bfe4f750b0
umarurize <3229187183@qq.com>
2025-07-10 00:51:45 +0800

    fix: you guess

--- src/endstone_ushop/__init__.py
+++ src/endstone_ushop/__init__.py
@@ -1,9 +1,9 @@
 from endstone_ushop.ushop import ushop
-from endstone_ushop.lang import load_langs
+from endstone_ushop.lang import load_lang
 from endstone_ushop.textures import load_textures
 
 __all__ = [
     'ushop',
-    'load_langs',
+    'load_lang',
     'load_textures'
 ]
